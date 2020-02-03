b5a6cbd78320e17282fce75b2bbfcff66a6cb4fd
IlyaM1 <57759494+IlyaM1@users.noreply.github.com>
2020-02-03 20:52:52 +0500

    Add files via upload

--- knopka.h
+++ knopka.h
@@ -1,4 +1,4 @@
-void drawEnterTextButton(int x1, int y1, int x2, int y2)
+void drawEnterTextButton(int x1, int y1, int x2, int y2)//рисование
 {
     txSetFillColour(TX_WHITE);
     txSetColour(TX_BLACK,5);
@@ -14,14 +14,16 @@ void drawEnterTextButton(int x1, int y1, int x2, int y2)
     txCircle(x3 + 5*r, y1 + (y2 - y1)/2, r);
 }
 
-const char* checkEnterTextButton(int x1, int y1, int x2, int y2,int font_size)
+const char* checkEnterTextButton(int x1, int y1, int x2, int y2,int font_size, const char* question, const char* caption)// проверка нажатия и ввод текста
 {
-    int x3 = x1 + ((x2-x1)*0.8);
-    RECT button = {x3,y1,x2,y2};
+
 
           const char* name;
-          name = txInputBox ("Whats your name?", "Enter something");
+          name = txInputBox (question, caption);
           txSetColour(TX_BLACK,font_size);
-          txTextOut(x1+2,y1+(y2-y1)/2,name);
+          txTextOut(x1+2,y1+(y2-y1)/2,name);// удачи
           return name;
+/*
+    x1,y1,x2,y2 - координаты, font_size - размер шрифта, question - что в окошечке будет писаться как вопрос, caption - в заголовке что будет писаться
+*/
 }
